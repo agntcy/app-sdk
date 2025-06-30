ff52cfb48d1d474c492c79d1585fd13a1d416b94
Aditi Gupta <54046563+therealaditigupta@users.noreply.github.com>
2025-06-30 13:38:34 -0400

    example of 'mcp over slim'
    Signed-off-by: Aditi Gupta <54046563+therealaditigupta@users.noreply.github.com>

--- tests/e2e/test_mcp.py
+++ tests/e2e/test_mcp.py
@@ -18,7 +18,7 @@ async def test_client():
 
     # Endpoint for local test mcp server
     endpoint = "http://localhost:8123/mcp"
-    transport = "StreamableHTTP"
+    transport = "SLIM"
 
     print(
         f"\n--- Starting test: test_client | Transport: {transport} | Endpoint: {endpoint} ---"
