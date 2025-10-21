ef28a5321e4c8a515356403a42e92591031ec4a3
Darren Lau <darrelau@cisco.com>
2025-10-21 18:41:35 -0400

    feat: support SLIM v0.6.0
    Signed-off-by: Darren Lau <darrelau@cisco.com>

--- src/agntcy_app_sdk/transport/slim/session_manager.py
+++ src/agntcy_app_sdk/transport/slim/session_manager.py
@@ -9,9 +9,8 @@ from agntcy_app_sdk.common.logging_config import configure_logging, get_logger
 import slim_bindings
 from slim_bindings import (
     PyName,
-    PySessionInfo,
+    PySession,
     PySessionConfiguration,
-    PySessionDirection,
 )
 from agntcy_app_sdk.semantic.message import Message
 
@@ -21,7 +20,7 @@ logger = get_logger(__name__)
 
 class SessionManager:
     def __init__(self):
-        self._sessions: Dict[str, PySessionInfo] = {}
+        self._sessions: Dict[str, PySession] = {}
         self._slim = None
         self._lock = asyncio.Lock()
 
@@ -31,8 +30,9 @@ class SessionManager:
         """
         self._slim = slim
 
-    async def request_reply_session(
+    async def point_to_point_session(
         self,
+        remote_name: PyName,
         max_retries: int = 5,
         timeout: datetime.timedelta = datetime.timedelta(seconds=5),
         mls_enabled: bool = True,
@@ -43,29 +43,23 @@ class SessionManager:
         if not self._slim:
             raise ValueError("SLIM client is not set")
 
-        # check if we already have a request-reply session
-        for session_id, (session, q) in self._slim.sessions.items():
-            try:
-                conf = await self._slim.get_session_config(session_id)
-                # compare the type of conf to PySessionConfiguration.FireAndForget
-                if isinstance(conf, PySessionConfiguration.FireAndForget):
-                    return session_id, session
-            except Exception as e:
-                # TODO: Revisit with SLIM team if this still exists in 0.5.0
-                logger.debug(
-                    f"could not retrieve SLIM session config for {session_id}: {e}"
-                )
-                continue
+        session_key = f"PySessionConfiguration.PointToPoint:{remote_name}"
+
+        if session_key in self._sessions:
+            logger.info(f"Reusing existing point-to-point session: {session_key}")
+            return self._sessions[session_key].id, self._sessions[session_key]
 
         async with self._lock:
             session = await self._slim.create_session(
-                PySessionConfiguration.FireAndForget(
+                PySessionConfiguration.PointToPoint(
+                    peer_name=remote_name,
                     max_retries=max_retries,
                     timeout=timeout,
-                    sticky=True,
                     mls_enabled=mls_enabled,
                 )
             )
+
+            self._sessions[session_key] = session
             return session.id, session
 
     async def group_broadcast_session(
@@ -83,9 +77,10 @@ class SessionManager:
             raise ValueError("SLIM client is not set")
 
         # check if we already have a group broadcast session for this channel and invitees
-        session_key = f"PySessionConfiguration.Streaming:{channel}:" + ",".join(
+        session_key = f"PySessionConfiguration.Group:{channel}:" + ",".join(
             [str(invitee) for invitee in invitees]
         )
+
         # use the same lock for session creation and lookup
         async with self._lock:
             if session_key in self._sessions:
@@ -93,11 +88,9 @@ class SessionManager:
                 return session_key, self._sessions[session_key]
 
             logger.debug(f"Creating new group broadcast session: {session_key}")
-            session_info = await self._slim.create_session(
-                PySessionConfiguration.Streaming(
-                    PySessionDirection.BIDIRECTIONAL,
-                    topic=channel,
-                    moderator=True,
+            group_session = await self._slim.create_session(
+                PySessionConfiguration.Group(
+                    channel_name=channel,
                     max_retries=max_retries,
                     timeout=timeout,
                     mls_enabled=mls_enabled,
@@ -106,21 +99,24 @@ class SessionManager:
 
             for invitee in invitees:
                 try:
-                    logger.debug(f"Inviting {invitee} to session {session_info.id}")
+                    logger.debug(f"Inviting {invitee} to session {group_session.id}")
                     await self._slim.set_route(invitee)
-                    await self._slim.invite(session_info, invitee)
+                    await group_session.invite(invitee)
                 except Exception as e:
                     logger.error(f"Failed to invite {invitee}: {e}")
 
             # store the session info
-            self._sessions[session_key] = session_info
-            return session_key, session_info
+            self._sessions[session_key] = group_session
+            return session_key, group_session
 
     async def close_session(
-        self, session: PySessionInfo, remote: PyName = None, end_signal: str = None
+        self, session: PySession, remote: PyName = None, end_signal: str = None
     ):
         """
-        Close and remove a session by its key.
+        Close and remove a session.
+        Args:
+            session (PySession): The PySession object to close.
+            end_signal (str, optional): An optional signal message to send before closing.
         """
         if not self._slim:
             raise ValueError("SLIM client is not set")
@@ -128,32 +124,33 @@ class SessionManager:
         try:
             # send the end signal to the remote if provided
             if remote is not None and end_signal is not None:
-                logger.info(f"Sending end signal '{end_signal}' to remote {remote}")
+                logger.info(f"Sending end signal '{end_signal}' to remote {session.dst}")
 
                 end_msg = Message(
                     type="text/plain",
                     headers={"x-session-end-message": end_signal},
                     payload=end_signal,
                 )
-                await self._slim.publish(session, end_msg.serialize(), remote)
+                await session.publish(end_msg.serialize())
 
-            logger.info(f"waiting before closing session: {session.id}")
+            logger.info(f"Waiting before attempting to delete session: {session.id}")
             # todo: proper way to wait for all messages to be processed
             await asyncio.sleep(
                 random.uniform(5, 10)
             )  # add sleep before closing to allow for any in-flight messages to be processed
-            logger.info(f"deleting session: {session.id}")
+            logger.info(f"Attempting to delete session: {session.id}")
 
             # Sometimes SLIM delete_session can hang indefinitely but still deletes the session, so we add a timeout
             try:
                 await asyncio.wait_for(
-                    self._slim.delete_session(session.id), timeout=5.0
+                    self._slim.delete_session(session), timeout=5.0
                 )
                 logger.info(
                     f"Session {session.id} deleted successfully within timeout."
                 )
             except asyncio.TimeoutError:
-                logger.info(f"Timed out while trying to delete session {session.id}.")
+                logger.warning(f"Timed out while trying to delete session {session.id}. "
+                               f"It might still have been deleted on SLIM server, but no confirmation was received.")
             except Exception as e:
                 logger.error(f"Error deleting session {session.id}: {e}")
             logger.info(f"Closed session: {session.id}")
@@ -178,6 +175,9 @@ class SessionManager:
             if session_key:
                 del self._sessions[session_key]
                 logger.debug(f"Locally cleaned up session: {session_id}")
+            else:
+                logger.warning(f"Session {session_id} cannot be removed from "
+                               f"local cache since this session was not found.")
 
     def session_details(self, session_key: str):
         """
